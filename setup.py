7818a6e8b5415233d0fba7431841c8e13b83baa0
tecnoexit <tecnoexit@gmail.com>
2024-02-05 23:18:26 -0300

    requerimientos

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name='nombre-de-tu-proyecto',
+    version='1.0.0',
+    description='Descripción de tu proyecto',
+    author='Tu Nombre',
+    install_requires=[
+        'beautifulsoup4',
+        'streamlit',
+    ],
+)
